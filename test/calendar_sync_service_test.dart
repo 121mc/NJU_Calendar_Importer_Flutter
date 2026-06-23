229144be6f9d8c6aa455d0d0202fbcda3d60e393
121mc <mc.121.kf@gmail.com>
2026-06-23 12:09:52 +0800

    test: declare calendar sync fake dependencies

--- test/calendar_sync_service_test.dart
+++ test/calendar_sync_service_test.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: depend_on_referenced_packages
-
 import 'package:device_calendar_plus/device_calendar_plus.dart';
 import 'package:device_calendar_plus_platform_interface/device_calendar_plus_platform_interface.dart';
 import 'package:flutter_test/flutter_test.dart';
